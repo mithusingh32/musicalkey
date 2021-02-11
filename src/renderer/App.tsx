36147a7526b6075d70e523a327d8cf4bac913c87
Sukhmeet Khalar <mithusingh32@gmail.com>
2021-02-11 12:27:28 -0500

    React structure added to electron

---
+++ src/renderer/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.global.css';
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello" />
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
