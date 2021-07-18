7d50ba052b11e27ddf5d2991b79f05d04f1f48f8
Sukhmeet Khalar <mithusingh32@gmail.com>
2021-07-18 11:22:28 -0400

    AAdding script that was missed in previous commit

---
+++ audio-processor/post-build.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const nodeModulePath = __dirname + "/build/Release/audio-processor.node";
+const destPath = __dirname + '/../src/audio-processor.node'
+
+// Check to see if file exists
+fs.access(nodeModulePath, fs.constants.F_OK, (err) => {
+    if (err) throw err
+});
+
+// Delete destination file if it exists
+if (fs.statSync(destPath, {throwIfNoEntry: false} !== undefined)) fs.unlinkSync(destPath)
+
+// If it exists, move it 
+fs.copyFileSync(nodeModulePath, destPath, fs.constants.COPYFILE_EXCL | fs.constants.COPYFILE_FICLONE_FORCE, (err) => {
+    if (err) throw err
+})
+
+console.log('Moved audio-processor.node to ' + destPath);
