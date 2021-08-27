98520464225e25f2d1b119b8df23e11dcf2f74d9
Sukhmeet Khalar <mithusingh32@gmail.com>
2021-08-27 17:07:01 -0400

    Fixed scss issue w/ yarn:build and added Redux back to the App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import store from './store/store';
 import * as Views from './views';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Views.Home} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Views.Home} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
