d6e21fae9e7ac507e052dfa34b84aa933d917c93
Sukhmeet Khalar <mithusingh32@gmail.com>
2022-01-14 14:45:58 -0500

    Added react-router back into App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import store from './store/store';
-import * as Views from './views';
 import { Home } from './views';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Provider>
   );
 };
